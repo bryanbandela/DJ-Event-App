7324aef9ab154948dc2144a6df728f829f0054c1
Bryan Bandela <bryan.bandela@gmail.com>
2021-08-12 17:53:28 +0200

    Pages and Routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page</p>
+      <p>This is where you will find all the events</p>
+    </div>
+  );
+}
